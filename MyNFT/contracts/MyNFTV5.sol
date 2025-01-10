cc7141eeb18cfe61d301eba1a727bd855c5b21c4
YooByWk <tls19190@naver.com>
2025-01-10 21:42:52 +0900

    style:

---
+++ MyNFT/contracts/MyNFTV5.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+// for holesky network
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract MyNFTV5 is ERC721URIStorageUpgradeable, OwnableUpgradeable {
+    uint256 private _tokenIds;
+
+    // v3
+    // mapping(address => uint256[]) private _ownedTokens;
+
+    function initialize() external initializer {
+        __ERC721URIStorage_init();
+        __ERC721_init("MyNFT", "YooNFT");
+        __Ownable_init(msg.sender);
+    }
+
+    function version() public pure returns (string memory) {
+        return "1.1.1.1";
+    }
+
+    function author() public pure returns (string memory) {
+        return "YBW_2025.01.09";
+    }
+
+    function mintNFT(
+        address recipient,
+        string memory tokenURI
+    ) public returns (uint256) {
+        _tokenIds++; // Solidity 0.8 이상에서는 오버플로우 체크가 자동임.
+        uint256 newTokenId = _tokenIds;
+
+        _mint(recipient, newTokenId); // 새로운 NFT 발행 (recipient, tokenId)
+        _setTokenURI(newTokenId, tokenURI); // 해당 tokenId에 대한 URI 설정
+
+        // _ownedTokens[recipient].push(newTokenId); // v3
+
+        return newTokenId; // 발행된 새로운 NFT의 tokenId 반환
+    }
+
+    function totalMintedNFTs() external view returns (uint256) {
+        return _tokenIds;
+    }
+
+    function getTokenURI(
+        uint256 tokenId
+    ) external view returns (string memory) {
+        return tokenURI(tokenId);
+    }
+
+    function canMintCheck() public pure returns (bool) {
+        return true;
+    }
+}
--- front/src/components/metamask/VersionBtn.tsx
+++ front/src/components/metamask/VersionBtn.tsx
@@ -12,6 +12,8 @@ const contractAddress = "0x0eeC786AF0C92a40E0B4D46D750fdd0a1fC8211F";
 const VersionBtn: React.FC<VersionBtnProps> = ({ provider, signer }) => {
   const checkVersion = async () => {
     const contract = new ethers.Contract(contractAddress, constractABI, signer);
+    
+    console.log(await contract.balanceOf("0x625fc12c23d6CE7C196900AaE0B1203369C04B2a"));
     console.log(contract);
     const res = await contract.version();
     console.log(res);
